5b9123ee6ed4a8c0d85f7beb595c03e860225c7c
nir9d <nir9dota@gmail.com>
2025-05-25 17:21:36 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,18 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list')
+function renderCard({name , link}) {
+    const templateCard = document.querySelector('#card-template').content
+    const cardElement = templateCard.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = name
+    cardElement.querySelector('.card__image').src = link 
+    const deleteButton = cardElement.querySelector('.card__delete-button')
+    deleteButton.addEventListener('click', deleteCard)
+    return cardElement
+}
+function deleteCard(){
+    const card = document.querySelector('.card')
+    card.remove()
+}
+initialCards.forEach(function(card){
+    const cardElement = renderCard(card)
+    placesList.append(cardElement)
+})
