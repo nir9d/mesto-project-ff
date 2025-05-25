cfae52713a707fb4cd99eee0e70765638524dd30
nir9d <nir9dota@gmail.com>
2025-05-25 18:17:24 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 const placesList = document.querySelector('.places__list')
 const templateCard = document.querySelector('#card-template').content
 function renderCard({name , link}) {
-    
     const cardElement = templateCard.querySelector('.places__item').cloneNode(true);
     cardElement.querySelector('.card__title').textContent = name
     cardElement.querySelector('.card__image').src = link 
