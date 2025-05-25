6ead01516b0b97e320f9557446cd6dd56c4a2635
nir9d <nir9dota@gmail.com>
2025-05-25 18:17:00 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,17 @@
 const placesList = document.querySelector('.places__list')
+const templateCard = document.querySelector('#card-template').content
 function renderCard({name , link}) {
-    const templateCard = document.querySelector('#card-template').content
+    
     const cardElement = templateCard.querySelector('.places__item').cloneNode(true);
     cardElement.querySelector('.card__title').textContent = name
     cardElement.querySelector('.card__image').src = link 
+    cardElement.querySelector('.card__image').alt = name
     const deleteButton = cardElement.querySelector('.card__delete-button')
     deleteButton.addEventListener('click', deleteCard)
     return cardElement
 }
-function deleteCard(){
-    const card = document.querySelector('.card')
-    card.remove()
+function deleteCard(evt){
+   evt.target.parentElement.remove()
 }
 initialCards.forEach(function(card){
     const cardElement = renderCard(card)
